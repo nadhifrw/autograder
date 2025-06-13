b1b994e3f9d463bb7eebe1974219bcd605c7cc30
nadhifrw <hernadhif.rawir@gmail.com>
2025-06-14 01:06:45 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,10 @@
+from checking_folder.checking_files import check_files_in_folder
+
+
+def main():
+    path = "/Users/yolashaniaanggita/Documents/test_files"
+    check_files_in_folder(path)
+
+
+if __name__ == "__main__":
+    main()
